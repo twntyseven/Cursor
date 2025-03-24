2d8346f31426dba341fce7d5fb1c9a2643996e29
twntyseven <theunrelistikgamer@gmail.com>
2025-03-24 09:00:41 -0500

    added 2nd page

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
+@app.route('/hello')
 def hello():
-   message = "Hello, Flask!"
+   page = """
+      <h1>Here's a random number: {0}</h1>
+      <form>
+         <button>New Number</button>
+      </form>
+   """
+   num = random.randint(1, 25)
+   return page.format(num)
+@app.route('/goodbye')
+def goodbye():
+   message = "<h2>This is the second page!</h2>"
    return message
 
 if __name__ == '__main__':
