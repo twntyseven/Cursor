98ce1970e31e32d6db7c0bbcb06b743f70fa8c16
twntyseven <theunrelistikgamer@gmail.com>
2025-03-27 09:03:25 -0500

    Thank you note page added

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
 
 @app.route('/')
 @app.route('/hello')
@@ -24,6 +25,15 @@ def third_page():
    message = "<h2>This is the third page!</h2>"
    return message
 
+@app.route('/form')
+def form():
+   return render_template("favorite_form.html")
+
+@app.route('/thanks')
+def thanks():
+   person = "Bob"
+   action = "dancing"
+   return render_template("tynote.html", name = person, verb = action)
 
 if __name__ == '__main__':
    app.run()
